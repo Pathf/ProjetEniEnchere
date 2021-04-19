8b524a6eea5d77b682416d019fb7ae63d30d8c90
maxim <maxim@192.168.1.11>
2021-04-19 17:06:33 +0200

    test michel

--- src/main/java/test/test.java
+++ src/main/java/test/test.java
@@ -29,7 +29,7 @@ public class test extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: lol ").append(request.getContextPath());
+		response.getWriter().append("Served at: michel ").append(request.getContextPath());
 	}
 
 	/**
