4687f577e24a6a6b2da00cc24d5efe7755f5abf7
Valentin <vhenique@gmail.com>
2021-04-19 17:19:56 +0200

    Dal enchere fait

--- src/main/java/org/encheres/bo/Enchere.java
+++ src/main/java/org/encheres/bo/Enchere.java
@@ -1,6 +1,6 @@
 package org.encheres.bo;
 
-import java.util.Date;
+import java.sql.Date;
 import java.util.Objects;
 
 public class Enchere {
--- src/main/java/org/encheres/dal/impl/EnchereDAOImpl.java
+++ src/main/java/org/encheres/dal/impl/EnchereDAOImpl.java
@@ -4,6 +4,8 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.encheres.bo.Enchere;
@@ -16,7 +18,7 @@ public class EnchereDAOImpl implements EnchereDAO {
 	private static String SQLSELECT_ID = "Select * from ENCHERES WHERE no_enchere=?";
 	private static String SQLSELECT_UTILISATEUR = "Select * from ENCHERES WHERE no_utilisateur=?";
 	//TODO
-	private static String SQLINSERT = "INSERT INTO ARTICLES_VENDUS (nom_article, description, date_debut_encheres, date_fin_encheres, prix_initial, prix_vente, no_utilisateur, no_categorie, no_retrait) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
+	private static String SQLINSERT = "INSERT INTO ENCHERES (no_enchere, date_enchere, montant_enchere, no_article, no_utilisateur) VALUES (?, ?, ?, ?, ?)";
 	private static String SQLUPDATE = "UPDATE ARTICLES_VENDUS SET nom_article=?, description=?, date_debut_encheres=?, date_fin_encheres=?, prix_initial=?, prix_vente=?, no_utilisateur=?, no_categorie=?, no_retrait=?";
 
 	@Override
@@ -48,7 +50,7 @@ public class EnchereDAOImpl implements EnchereDAO {
 
 	@Override
 	public List<Enchere> selectUtilisateur(Integer id_utilisateur) throws DALException {
-		/*List<ArticleVendu> articles = new ArrayList<>();
+		List<Enchere> encheres = new ArrayList<>();
 		try (	Connection connection = DAOTools.getConnection();
 				Statement statement = connection.createStatement();
 				) {
@@ -56,73 +58,58 @@ public class EnchereDAOImpl implements EnchereDAO {
 
 			try (ResultSet rs = statement.getResultSet();){
 				if(rs.next()){
-					articles.add(new ArticleVendu(
-							rs.getInt("id"),
-							rs.getString("nom_article").trim(),
-							rs.getString("description"),
-							rs.getDate("date_debut_encheres"),
-							rs.getDate("date_fin_encheres"),
-							rs.getInt("prix_initial"),
-							rs.getInt("prix_vente"),
-							rs.getInt("no_utilisateur"),
-							rs.getInt("no_categorie"),
-							rs.getInt("no_retrait")
+					encheres.add(new Enchere(
+							rs.getInt("no_enchere"),
+							rs.getDate("date_enchere"),
+							rs.getInt("montant_enchere"),
+							rs.getInt("no_article"),
+							rs.getInt("no_utilisateur")
 							));
 				}
 			}catch (SQLException e) {
-				throw new DALException("Select ALL failed - close failed for rs -  ", e);
+				throw new DALException("Select utilisateur failed - close failed for rs -  ", e);
 			}
 		} catch (SQLException e) {
-			throw new DALException("Select All failed - ", e);
-		}*/
-		return null;
+			throw new DALException("Select utilisateur failed - ", e);
+		}
+		return encheres;
 	}
-
+	// no_enchere, date_enchere, montant_enchere, no_article, no_utilisateur
 	@Override
-	public void insert(Enchere articleVendu) throws DALException {
-		/*try (	Connection connection = DAOTools.getConnection();
+	public void insert(Enchere enchere) throws DALException {
+		try (	Connection connection = DAOTools.getConnection();
 				PreparedStatement preparedStatement = connection.prepareStatement(SQLINSERT, Statement.RETURN_GENERATED_KEYS);
 				){
-			preparedStatement.setString(1, articleVendu.getNom_article());
-			preparedStatement.setString(2, articleVendu.getDescription());
-			preparedStatement.setDate(3, articleVendu.getDate_debut_encheres());
-			preparedStatement.setDate(4, articleVendu.getDate_fin_encheres());
-			preparedStatement.setInt(5, articleVendu.getPrix_initial());
-			preparedStatement.setInt(6, articleVendu.getPrix_vente());
-			preparedStatement.setInt(7, articleVendu.getNo_utilisateur());
-			preparedStatement.setInt(8, articleVendu.getNo_categorie());
-			preparedStatement.setInt(9, articleVendu.getNo_retrait());
+			preparedStatement.setDate(1, enchere.getDate_enchere());
+			preparedStatement.setInt(2, enchere.getMontant_enchere());
+			preparedStatement.setInt(3, enchere.getNo_article());
+			preparedStatement.setInt(4, enchere.getNo_utilisateur());
 
 			preparedStatement.executeUpdate();
 
 			try(ResultSet rs = preparedStatement.getGeneratedKeys()){
-				articleVendu.setNo_article(rs.getInt(1));
+				enchere.setNo_article(rs.getInt(1));
 			} catch (SQLException e) {
-				throw new DALException("Insert articleVendu return key failed - " + articleVendu + " - ", e);
+				throw new DALException("Insert enchere return key failed - " + enchere + " - ", e);
 			}
 		} catch (SQLException e) {
-			throw new DALException("Insert articleVendu failed - " + articleVendu + " - ", e);
-		}*/
+			throw new DALException("Insert enchere failed - " + enchere + " - ", e);
+		}
 	}
 
 	@Override
-	public void update(Enchere articleVendu) throws DALException {
-		/*try (	Connection connection = DAOTools.getConnection();
+	public void update(Enchere enchere) throws DALException {
+		try (	Connection connection = DAOTools.getConnection();
 				PreparedStatement preparedStatement = connection.prepareStatement(SQLUPDATE);
 				){
-			preparedStatement.setString(1, articleVendu.getNom_article());
-			preparedStatement.setString(2, articleVendu.getDescription());
-			preparedStatement.setDate(3, articleVendu.getDate_debut_encheres());
-			preparedStatement.setDate(4, articleVendu.getDate_fin_encheres());
-			preparedStatement.setInt(5, articleVendu.getPrix_initial());
-			preparedStatement.setInt(6, articleVendu.getPrix_vente());
-			preparedStatement.setInt(7, articleVendu.getNo_utilisateur());
-			preparedStatement.setInt(8, articleVendu.getNo_categorie());
-			preparedStatement.setInt(9, articleVendu.getNo_retrait());
+			preparedStatement.setDate(1, enchere.getDate_enchere());
+			preparedStatement.setInt(2, enchere.getMontant_enchere());
+			preparedStatement.setInt(3, enchere.getNo_article());
+			preparedStatement.setInt(4, enchere.getNo_utilisateur());
 
 			preparedStatement.executeUpdate();
 		} catch (SQLException e) {
-			throw new DALException("Update articleVendu failed - " + articleVendu + " - ", e);
-		}*/
+			throw new DALException("Update enchere failed - " + enchere + " - ", e);
+		}
 	}
 }
