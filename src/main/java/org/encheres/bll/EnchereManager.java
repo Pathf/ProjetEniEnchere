f2657f2bb324d8779690f5d380b1636bc9537d5e
Valentin <vhenique@gmail.com>
2021-04-20 15:57:35 +0200

    Création BLL enchereManager et son exception

---
+++ src/main/java/org/encheres/bll/EnchereManager.java
@@ -0,0 +1,80 @@
+package org.encheres.bll;
+
+import java.util.List;
+
+import org.encheres.bo.Enchere;
+import org.encheres.bo.Utilisateur;
+import org.encheres.dal.DALException;
+import org.encheres.dal.FactoryDAO;
+import org.encheres.dal.dao.EnchereDAO;
+
+public class EnchereManager {
+	private static EnchereManager enchereManager;
+	private EnchereDAO enchereDAO;
+
+	private EnchereManager() {
+		this.enchereDAO = FactoryDAO.getEnchere();
+	}
+
+	public static EnchereManager getInstance() {
+		if(enchereManager == null) {
+			enchereManager = new EnchereManager();
+		}
+		return enchereManager;
+	}
+
+	public Enchere getEnchere(Integer id) throws EnchereManagerException {
+		Enchere enchere = null;
+		try {
+			enchere = this.enchereDAO.selectById(id);
+		} catch (DALException e) {
+			throw new EnchereManagerException("getEnchere failed - ", e);
+		}
+		return enchere;
+	}
+
+	public List<Enchere> getListeEnchere(Integer id_utilisateur) throws EnchereManagerException {
+		List<Enchere> encheres = null;
+		try {
+			encheres = this.enchereDAO.selectUtilisateur(id_utilisateur);
+		} catch (DALException e) {
+			throw new EnchereManagerException("getListeEnchere failed - ", e);
+		}
+		return encheres;
+	}
+
+	public void addEnchere(Enchere enchere) throws EnchereManagerException {
+		try {
+			this.enchereDAO.insert(enchere);
+		} catch (DALException e) {
+			throw new EnchereManagerException("addEnchere failed - ", e);
+		}
+	}
+
+	public void updateEncher(Enchere nouvelleEnchere) throws EnchereManagerException {
+		try {
+			// select encheractuelle
+			Enchere encherePrecedente = this.enchereDAO.selectById(nouvelleEnchere.getNo_enchere());
+			// verifie si l'enchere proposé est strictement supérieur a l'actuelle
+			if(nouvelleEnchere.getMontant_enchere() > encherePrecedente.getMontant_enchere() &&
+					nouvelleEnchere.getUtilisateur().getCredit() >= nouvelleEnchere.getMontant_enchere()) {
+				// recredite l'ancien encherisseur
+				Utilisateur utilisateurPrecedenteEnchere = encherePrecedente.getUtilisateur();
+				utilisateurPrecedenteEnchere.setCredit(utilisateurPrecedenteEnchere.getCredit() + encherePrecedente.getMontant_enchere());
+				UtilisateurManager.getInstance().updateUtilisateur(utilisateurPrecedenteEnchere);
+
+				// decredite le nouveau encherisseur
+				Utilisateur utilisateurNouvelleEnchere = nouvelleEnchere.getUtilisateur();
+				utilisateurNouvelleEnchere.setCredit(utilisateurNouvelleEnchere.getCredit() - nouvelleEnchere.getMontant_enchere());
+				UtilisateurManager.getInstance().updateUtilisateur(utilisateurNouvelleEnchere);
+
+				// update => insert d'une nouvelle enchere qui devient l'actuel
+				this.enchereDAO.insert(nouvelleEnchere);
+			}
+		} catch (DALException e) {
+			throw new EnchereManagerException("updateEncher failed - ", e);
+		} catch (UtilisateurManagerException e) {
+			throw new EnchereManagerException("updateEncher failed - updateUtilisateur failed - ", e);
+		}
+	}
+}
---
+++ src/main/java/org/encheres/bll/EnchereManagerException.java
@@ -0,0 +1,24 @@
+package org.encheres.bll;
+
+public class EnchereManagerException extends Exception {
+	private static final long serialVersionUID = 3640154309408899123L;
+
+	public EnchereManagerException() {
+		super();
+	}
+
+	public EnchereManagerException(String message) {
+		super(message);
+	}
+
+	public EnchereManagerException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	@Override
+	public String getMessage() {
+		StringBuffer sb = new StringBuffer("Couche BLL EnchereManagerException - ");
+		sb.append(super.getMessage());
+		return sb.toString();
+	}
+}
