0b1fd05aa0671d7390492d9ae19f16ba411caf4c
Valentin <vhenique@gmail.com>
2021-04-21 17:42:28 +0200

    Mise en place de la deco

--- WebContent/WEB-INF/jsp/accueil.jsp
+++ WebContent/WEB-INF/jsp/accueil.jsp
@@ -1,12 +1,12 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
 <!DOCTYPE html>
 <html>
-<head>
-<meta charset="UTF-8">
-<title>Accueil</title>
-</head>
+<jsp:include page="/WEB-INF/fragments/head.jsp"></jsp:include>
 <body>
+<% String isConnecte = "1"; %>
+<%@include file="/WEB-INF/fragments/navbar.jspf"%>
 	<p>C'est l'accueil !</p>
 	<% String pseudo = (String)session.getAttribute("pseudo"); %>
 	<%=pseudo%>
---
+++ src/org/encheres/ihm/DeconnexionServlet.java
@@ -0,0 +1,23 @@
+package org.encheres.ihm;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/deconnexion")
+public class DeconnexionServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.getSession().invalidate();
+		System.out.println(request.getSession());
+		request.getRequestDispatcher("/WEB-INF/jsp/accueil.jsp").forward(request, response);
+		//response.sendRedirect(request.getContextPath()+"/connexion");
+	}
+
+}
