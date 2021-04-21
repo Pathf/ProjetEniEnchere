7f19300b575ebadbd2c1f8e947ac7dd74b8e41c0
Valentin <vhenique@gmail.com>
2021-04-22 00:38:37 +0200

    Suppression d'espace et d'import inutile

--- src/org/encheres/bll/CategoriesManager.java
+++ src/org/encheres/bll/CategoriesManager.java
@@ -2,18 +2,12 @@ package org.encheres.bll;
 
 import java.util.List;
 
-import org.encheres.bo.ArticleVendu;
 import org.encheres.bo.Categorie;
 import org.encheres.dal.DALException;
 import org.encheres.dal.FactoryDAO;
-import org.encheres.dal.dao.ArticleVenduDAO;
 import org.encheres.dal.dao.CategorieDAO;
-import org.encheres.dal.dao.RetraitDAO;
-import org.encheres.dal.impl.CategorieDAOImpl;
 
 public class CategoriesManager {
-
-	
 	private static CategoriesManager categorieManager;
 	private CategorieDAO categorieDAO;
 
@@ -27,17 +21,14 @@ public class CategoriesManager {
 		}
 		return categorieManager;
 	}
-	
-	
+
 	public List<Categorie> getListeCategorie() throws CategorieManagerException {
 		List<Categorie> categories = null;
-
 		try {
 			categories = this.categorieDAO.selectAll();
 		} catch (DALException e) {
 			throw new CategorieManagerException("getListeCategorie failed - ", e);
 		}
-
 		return categories;
 	}
 }
