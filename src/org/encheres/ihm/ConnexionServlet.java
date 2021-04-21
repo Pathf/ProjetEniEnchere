dfbec1951a8dfa7c756cc1059c8d248af1fca450
Valentin <vhenique@gmail.com>
2021-04-21 20:57:12 +0200

    Rien - enlever un commentaire

--- src/org/encheres/ihm/ConnexionServlet.java
+++ src/org/encheres/ihm/ConnexionServlet.java
@@ -48,7 +48,6 @@ public class ConnexionServlet extends HttpServlet {
 		}
 		RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/jsp/accueil.jsp");
 		response.sendRedirect("encheres");
-//		rd.forward(request, response);
 	}
 
 }
