2d0057c3991130d15cb80d1eb133cdc479d6e7f5
Valentin <vhenique@gmail.com>
2021-04-22 11:01:09 +0200

    Creation du servlet de suppression

---
+++ src/org/encheres/ihm/SuppressionServlet.java
@@ -0,0 +1,80 @@
+package org.encheres.ihm;
+
+import java.io.IOException;
+import java.sql.Date;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.encheres.bll.ArticleVenduManager;
+import org.encheres.bll.ArticleVenduManagerException;
+import org.encheres.bll.EnchereManager;
+import org.encheres.bll.EnchereManagerException;
+import org.encheres.bll.UtilisateurManager;
+import org.encheres.bll.UtilisateurManagerException;
+import org.encheres.bo.ArticleVendu;
+import org.encheres.bo.Enchere;
+import org.encheres.bo.Utilisateur;
+
+@WebServlet("/suppression")
+public class SuppressionServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private EnchereManager enchereManager = EnchereManager.getInstance();
+	private UtilisateurManager utilisateurManager = UtilisateurManager.getInstance();
+	private ArticleVenduManager articleVenduManager = ArticleVenduManager.getInstance();
+
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// suppression du compte utilisateur :
+		HttpSession session = request.getSession();
+		String pseudo = (String)session.getAttribute("pseudo");
+		if(pseudo != null && !pseudo.isEmpty()) {
+			// suppression que si pas d'encheres en cours
+			Utilisateur utilisateur;
+			try {
+				utilisateur = this.utilisateurManager.getUtilisateur(pseudo);
+
+				List<Enchere> encheres = this.enchereManager.getListeEnchere(utilisateur.getNo_utilisateur());
+				boolean enchereEnCour = false;
+				for(Enchere enchere : encheres) {
+					Date dateNow = new Date(System.currentTimeMillis());
+					Date dateFinEnchere = enchere.getArticle().getDate_fin_encheres();
+					if(dateNow.before(dateFinEnchere)) {
+						enchereEnCour = true;
+						break;
+					}
+				}
+				if(!enchereEnCour) {
+					// modifier tous les article possedant le no_utilisateur correspondant à l'utilisateur
+					Utilisateur utilisateurInconnu = this.utilisateurManager.getUtilisateur("inconnu");
+					List<ArticleVendu> articleVendus = this.articleVenduManager.getListeArticleVenduByUtilisateur(utilisateur.getNo_utilisateur());
+					for(ArticleVendu articleVendu : articleVendus) {
+						articleVendu.setUtilisateur(utilisateurInconnu);
+						this.articleVenduManager.updateArticleVendu(articleVendu);
+					}
+					// modifier toutes les encheres possedant le no_utilisateur correspondant à l'utilisateur
+					for(Enchere enchere : encheres) {
+						enchere.setUtilisateur(utilisateurInconnu);
+						this.enchereManager.updateEncher(enchere);
+					}
+					// suppression utilisateur
+					this.utilisateurManager.delete(utilisateur);
+				} else {
+					System.out.println("Erreur : il reste des encheres en cours");
+				}
+			} catch (UtilisateurManagerException | EnchereManagerException | ArticleVenduManagerException e) {
+				System.out.println(e);
+			}
+
+
+
+
+		}
+	}
+
+}
