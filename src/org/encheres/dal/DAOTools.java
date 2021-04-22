79aa5278c3a803da6496d567af632b48f1d168d8
Valentin <vhenique@gmail.com>
2021-04-22 17:53:32 +0200

    Passage en pool de connexion

--- src/org/encheres/dal/DAOTools.java
+++ src/org/encheres/dal/DAOTools.java
@@ -1,23 +1,34 @@
 package org.encheres.dal;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
 
+import javax.naming.Context;
+import javax.naming.NamingException;
+import javax.naming.directory.InitialDirContext;
+import javax.sql.DataSource;
+
 public class DAOTools {
+	private static DataSource dataSource;
 	private static String urldb;
 	private static String userdb;
-	private static String passworddb;
+	//private static String passworddb;
 
 	static {
 		try {
+			Context context = new InitialDirContext();
+			dataSource = (DataSource) context.lookup("java:comp/env/jdbc/pool_cnx");
+		} catch (NamingException e) {
+			e.printStackTrace();
+		}
+		/*try {
 			Class.forName(Settings.getProperties("driverdb"));
 		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
-		}
+		}*/
 		urldb = Settings.getProperties("urldb");
 		userdb = Settings.getProperties("userdb");
-		passworddb = Settings.getProperties("passworddb");
+		//passworddb = Settings.getProperties("passworddb");
 
 		System.out.println("--------------------------");
 		System.out.println("Connexion à la base de donnée TROCENCHERES");
@@ -28,6 +39,10 @@ public class DAOTools {
 	}
 
 	public static Connection getConnection() throws SQLException {
-		return  DriverManager.getConnection(urldb,userdb,passworddb);
+		return dataSource.getConnection();
 	}
+
+	/*public static Connection getConnection() throws SQLException {
+		return  DriverManager.getConnection(urldb,userdb,passworddb);
+	}*/
 }
