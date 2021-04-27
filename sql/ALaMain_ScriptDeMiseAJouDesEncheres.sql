d125b2d136b7a8053e679f09f0315c5339c2ad1d
Valentin <vhenique@gmail.com>
2021-04-27 12:27:42 +0200

    MAJ des fichiers sql et création du script de montée de version

--- sql/ALaMain_ScriptDeMiseAJouDesEncheres.sql
+++ sql/ALaMain_ScriptDeMiseAJouDesEncheres.sql
@@ -1,6 +1,8 @@
+-- Version SQL seulement
 UPDATE ARTICLES_VENDUS SET prix_vente = tmp.montant FROM ARTICLES_VENDUS AS a INNER JOIN (SELECT e.no_article, max(montant_enchere) AS montant FROM ENCHERES AS e LEFT JOIN ARTICLES_VENDUS AS a ON e.no_article = a.no_article GROUP BY e.no_article) AS tmp ON a.no_article = tmp.no_article WHERE a.date_fin_encheres <= GETDATE() AND a.prix_vente IS NULL;
 
 
+-- Version Transact-SQL en activant le job préalablement crééer
 USE msdb ;  
 GO
 
