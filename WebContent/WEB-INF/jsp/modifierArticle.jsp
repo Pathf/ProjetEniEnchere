52adc78584a7294f7d82b02c34dc10ce3d01d1d1
SimDub <duboelle.s@gmail.com>
2021-04-29 11:20:01 +0200

    creation servle et jsp modifier un article

---
+++ WebContent/WEB-INF/jsp/modifierArticle.jsp
@@ -0,0 +1,126 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/fmt" prefix="fmt"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<!DOCTYPE html>
+<% String erreur = (String)request.getAttribute("erreur"); %>
+<html>
+<jsp:include page="/WEB-INF/fragments/head.jsp">
+	<jsp:param value="./css/vendreUnArticle.css" name="style"/>
+</jsp:include>
+<body>
+	<%@ include file="/WEB-INF/fragments/navbar.jspf" %>
+	<div class="container">
+		<div class="mx-auto text-center">
+			<h1>${ article.getNom_article() }</h1>
+		</div>
+		<c:if test="${ erreur != null }">
+			<div class="alert alert-danger alert-dismissible fade show mx-auto col-md-10 offset-md-1 mt-3" role="alert">
+			  <strong>Erreur !</strong> ${erreur}
+			  <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+			    <span aria-hidden="true">&times;</span>
+			  </button>
+			</div>
+		</c:if>
+		<div class="row mt-5">
+			<div class="photo d-none d-md-block col-md-4">
+				<c:choose>
+					<c:when test="${article.getPhotoNom() != null}">
+						<a href="images?id=${article.getNo_article()}"><img alt="Pas de photo disponible." class="img-thumbnail col-12" src="images?id=${article.getNo_article()}"></a>
+					</c:when>
+					<c:otherwise>
+						<img alt="Pas de photo disponible." class="img-thumbnail col-12" src="https://via.placeholder.com/150	C/O https://placeholder.com/">
+					</c:otherwise>
+				</c:choose>
+			</div>
+			<div class="formulaire col-md-6 ml-md-3">
+				<form action="modifier-vente?id=${article.getNo_article() }" method="post" enctype="multipart/form-data">
+			   		<div class="form-group row">
+					    <label for="article" class="col-4 col-md-3 col-form-label">Article :</label>
+					    <div class="col-8 col-md-8">
+					      <input type="text" class="form-control" id="article" name="article" value="${ article.getNom_article() }">
+					    </div>
+				  	</div>
+				  	<div class="form-group row">
+					    <label for="description" class="col-4 col-md-3 col-form-label">Description :</label>
+					    <div class="col-8 col-md-8">
+					      <textarea class="form-control" id="description" rows="3" name="description">${ article.getDescription() }</textarea>
+					    </div>
+			  		</div>
+			  		<div class="form-group row">
+					    <label for="categorie" class="col-4 col-md-4 col-form-label">Catégorie</label>
+					    <div class="col-8 col-md-7">
+				      		<select class="form-control" id="exampleFormControlSelect1" name="categorie">
+						      <option value="${ article.getCategorie().getNo_categorie() }" selected>${ article.getCategorie().getLibelle() }</option>
+						      <c:forEach var="c" items="${categories}">
+						      	<option value="${ c.getNo_categorie() }">${ c.getLibelle() }</option>
+						      </c:forEach>
+					    	</select>
+					    </div>
+			  		</div>
+			  		<div class="form-group row">
+					    <label for="photoArticle" class="col-5 col-md-4 col-form-label">Photo de l'article</label>
+					    <div class="col-7 col-md-7">
+				      		<input type="file" class="form-control-file col-12" id="photoArticle" name="photoArticle">
+					    </div>
+			  		</div>
+			  		<div class="photo d-md-none col-12 mb-3">
+						<c:choose>
+							<c:when test="${article.getPhotoNom() != null}">
+								<a href="images?id=${article.getNo_article()}"><img alt="Pas de photo disponible." class="img-thumbnail col-12" src="images?id=${article.getNo_article()}"></a>
+							</c:when>
+							<c:otherwise>
+								<img alt="Pas de photo disponible." class="img-thumbnail col-12" src="https://via.placeholder.com/150	C/O https://placeholder.com/">
+							</c:otherwise>
+				</c:choose>
+					</div>
+			  		<div class="form-group row">
+					    <label for="miseAPrix" class="col-4 col-md-4 col-form-label">Mise à prix :</label>
+					    <div class="col-7 col-md-7">
+				      		<input type="number" id="miseAPrix" class="form-control" name="miseAPrix" value="${ article.getPrix_initial() }"/>
+					    </div>
+			  		</div>
+			  		<div class="form-group row">
+					    <label for="debutEnchere" class="col-4 col-md-4 col-form-label">Début de l'enchère :</label>
+					    <div class="col-7 col-md-7">
+				      		<input class="form-control" type="date" id="debutEnchere" name="debutEnchere" value="${article.getDate_debut_encheres()}">
+					    </div>
+			  		</div>
+			  		<div class="form-group row">
+					    <label for="finEnchere" class="col-4 col-md-4 col-form-label">Fin de l'enchère :</label>
+					    <div class="col-7 col-md-7">
+				      		<input class="form-control" type="date" id="finEnchere" name="finEnchere" value="${article.getDate_fin_encheres()}">
+					    </div>
+			  		</div>
+			  		<fieldset class="scheduler-border">
+			  			<legend class="scheduler-border">Retrait</legend>
+				  		<div class="form-group row">
+						    <label for="rue" class="col-4 col-md-4 col-form-label">Rue :</label>
+						    <div class="col-7 col-md-7">
+						      <input type="text" class="form-control" id="rue" name="rue" value="${ article.getRetrait().getRue() }">
+						    </div>
+				  		</div>
+				  		<div class="form-group row">
+						    <label for="codePostal" class="col-4 col-md-4 col-form-label">Code postal :</label>
+						    <div class="col-7 col-md-7">
+						      <input type="text" class="form-control" id="codePostal" name="codePostal" value="${ article.getRetrait().getCode_postal() }">
+						    </div>
+				  		</div>
+				  		<div class="form-group row">
+						    <label for="ville" class="col-4 col-md-4 col-form-label">Ville :</label>
+						    <div class="col-7 col-md-7">
+						      <input type="text" class="form-control" id="ville" name="ville" value="${ article.getRetrait().getVille() }">
+						    </div>
+				  		</div>
+			  		</fieldset>
+			  		<div class="mb-5">
+						<button type="submit" class="btn btn-primary mr-md-3 mb-3 mb-md-0 p-3 col-md-4">Enregistrer</button>
+						<a href="${pageContext.request.contextPath}/detail-enchere?id=${article.getNo_article()}" class="btn btn-danger ml-md-3 p-3 col-md-4" role="button">Annuler</a>
+					</div>
+				</form>
+			</div>
+		</div>
+	</div>
+</body>
+<%@ include file="/WEB-INF/fragments/script.html" %>
+</html>
---
+++ src/org/encheres/ihm/ModifierArticle.java
@@ -0,0 +1,189 @@
+package org.encheres.ihm;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Date;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import javax.servlet.http.Part;
+
+import org.encheres.bll.ArticleVenduManager;
+import org.encheres.bll.ArticleVenduManagerException;
+import org.encheres.bll.CategorieManagerException;
+import org.encheres.bll.CategoriesManager;
+import org.encheres.bll.UtilisateurManager;
+import org.encheres.bll.UtilisateurManagerException;
+import org.encheres.bo.ArticleVendu;
+import org.encheres.bo.Categorie;
+import org.encheres.bo.Retrait;
+import org.encheres.bo.Utilisateur;
+
+/**
+ * Servlet implementation class ModifierArticle
+ */
+@WebServlet("/modifier-vente")
+@MultipartConfig(maxFileSize = 16177215)    // upload file's size up to 16MB
+public class ModifierArticle extends HttpServlet {
+	private ArticleVenduManager articleVenduManager = ArticleVenduManager.getInstance();
+	private CategoriesManager categoriesManager = CategoriesManager.getInstance();
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		HttpSession session = request.getSession();
+		String pseudo = (String) session.getAttribute("pseudo");
+		Integer articleId = Integer.parseInt(request.getParameter("id"));
+		ArticleVendu articleVendu = null;
+		List<Categorie> categories = null;
+		Date dateActuelle = new Date(new java.util.Date().getTime());
+		
+		try {
+			articleVendu = this.articleVenduManager.getArticleVendu(articleId);
+		} catch (ArticleVenduManagerException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		if (pseudo.equals(articleVendu.getUtilisateur().getPseudo()) && dateActuelle.before(articleVendu.getDate_debut_encheres())) {
+			try {
+				categories = this.categoriesManager.getListeCategorie();
+				request.setAttribute("categories", categories);
+				request.setAttribute("article", articleVendu);
+			} catch (CategorieManagerException e) {
+				System.err.println(e);
+			}
+
+			request.getRequestDispatcher("/WEB-INF/jsp/modifierArticle.jsp").forward(request, response);
+		} else {
+			response.sendRedirect(request.getContextPath());
+		}
+
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		HttpSession session = request.getSession();
+		String pseudo = (String) session.getAttribute("pseudo");
+		ArticleVendu articleVendu = null;
+		Date dateActuelle = new Date(new java.util.Date().getTime());
+		Integer articleId = Integer.parseInt(request.getParameter("id"));
+		
+		try {
+			articleVendu = this.articleVenduManager.getArticleVendu(articleId);
+		} catch (ArticleVenduManagerException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		if (pseudo.equals(articleVendu.getUtilisateur().getPseudo()) && dateActuelle.before(articleVendu.getDate_debut_encheres())) {
+			Categorie categorie = null;
+			String erreur = null;
+			Date debutEnchere = null;
+			Date finEnchere = null;
+			String article = request.getParameter("article");
+			String description = request.getParameter("description");
+			Integer categorieId = Integer.parseInt(request.getParameter("categorie"));
+			String rue = request.getParameter("rue");
+			String codePostal = request.getParameter("codePostal");
+			String ville = request.getParameter("ville");
+			String miseAPrixString = request.getParameter("miseAPrix");
+			Part filePart = request.getPart("photoArticle");
+			String photoNom = null;
+			byte[] photoData = null;
+			if(filePart != null) {
+				photoNom = filePart.getSubmittedFileName();
+				photoData = this.toByteArray(filePart.getInputStream());
+			}
+			try {
+				debutEnchere = new Date(new SimpleDateFormat("yyyy-MM-dd").parse(request.getParameter("debutEnchere")).getTime());
+			} catch (ParseException e) {
+				System.err.println(e);
+			}
+			try {
+				finEnchere = new Date(new SimpleDateFormat("yyyy-MM-dd").parse(request.getParameter("finEnchere")).getTime());
+			} catch (ParseException e) {
+				System.err.println(e);
+			}
+			
+			
+			
+			
+			if (miseAPrixString != null && !miseAPrixString.isEmpty()) {
+				Integer miseAPrix = Integer.parseInt(miseAPrixString);
+				if(		article != null && !article.isEmpty() &&
+						description != null && !description.isEmpty() &&
+						categorieId != null &&
+						debutEnchere != null &&
+						rue != null && !rue.isEmpty() &&
+						codePostal != null && !codePostal.isEmpty() &&
+						ville != null && !ville.isEmpty()
+						) {
+					if(finEnchere != null && debutEnchere != null) {
+						if (finEnchere.after(debutEnchere) && (debutEnchere.after(dateActuelle) || debutEnchere.compareTo(dateActuelle)==0)) {
+							try {
+								categorie = this.categoriesManager.getCategorie(categorieId);
+							} catch (CategorieManagerException e) {
+								System.err.println(e);
+							}
+							
+							articleVendu.setNom_article(article);
+							articleVendu.setDescription(description);
+							articleVendu.setCategorie(categorie);
+							articleVendu.setPrix_initial(miseAPrix);
+							articleVendu.setDate_debut_encheres(debutEnchere);
+							articleVendu.setDate_fin_encheres(finEnchere);
+							articleVendu.getRetrait().setRue(rue);
+							articleVendu.getRetrait().setCode_postal(codePostal);
+							articleVendu.getRetrait().setVille(ville);
+							if (photoNom != null && !photoNom.isEmpty()) {
+								articleVendu.setPhotoNom(photoNom);
+								articleVendu.setPhotoData(photoData);
+							}
+							
+							try {
+								this.articleVenduManager.updateArticleVendu(articleVendu);
+								response.sendRedirect(request.getContextPath()+"/detail-enchere?id="+articleVendu.getNo_article());
+								return;
+							} catch (ArticleVenduManagerException e) {
+								System.err.println(e);
+								erreur = "Une erreur est survenue !";
+							}
+						} else {
+							erreur = "Veuillez enregistrer des dates correctes !";
+						}
+					} else {
+						erreur = "Les dates de l'enchère sont obligatoires !";
+					}
+				} else {
+					erreur = "Vous avez oublié de remplir un champ !";
+				}
+			} else {
+				erreur = "La mise à prix est obligatoire !";
+			}
+			request.setAttribute("erreur", erreur);
+			this.doGet(request, response);
+		} else {
+			response.sendRedirect(request.getContextPath());
+		}
+	}
+
+	private byte[] toByteArray(InputStream inputStream) throws IOException {
+		ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+		int nRead;
+		byte[] data = new byte[1024];
+		while ((nRead = inputStream.read(data, 0, data.length)) != -1) {
+			buffer.write(data, 0, nRead);
+		}
+
+		buffer.flush();
+		return buffer.toByteArray();
+	}
+}
